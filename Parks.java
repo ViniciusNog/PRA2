5526d29e04242755bc048d65b234c5af39e0d6c1
ViniciusNog <65199022+ViniciusNog@users.noreply.github.com>
2020-05-11 20:30:09 -0300

    Create Parks.java

---
+++ Parks.java
@@ -0,0 +1,99 @@
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+@Entity(tableName = "parks")//criação da entidade
+
+public class Parks{
+
+	@PrimaryKey(autoGenerate = true)
+  
+	@NonNull
+  
+	@ColumnInfo(name = "parksId")
+  
+	private int mId; //identificador único
+
+	@Nullable
+
+	@ColumnInfo(name = "parkstCreationDate")
+
+	private String mCreationDate;//data de controle para o BD
+
+	@Nullable
+
+	@ColumnInfo(name = "parksTitle")
+
+	private String mName; //nome do parque
+
+	@Nullable
+
+	@ColumnInfo(name = "parksLocation")
+
+	private String mLocation; //nome do lugar onde está o parque
+
+	@Nullable
+
+	@ColumnInfo(name = "parksScore")
+
+	private int mScore; //nota do parque de 0 à 10
+
+	public Parks(@Nullable String creationdate, String name, int score, String location) {
+
+    mCreationDate = creationdate;
+
+    mName = name;
+
+    mLocation = location;
+
+    mScore = score;
+	}
+  
+	//Gerando getters and setters
+
+	public int getId() {
+		return mId;
+	}
+
+	public void setId(int mId) {
+		mId = Id;
+	}
+
+	public String getCreationDate() {
+		return mCreationDate;
+	}
+
+	public void setmCreationDate(String CreationDate) {
+		mCreationDate = CreationDate;
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public void setName(String mName) {
+		mName = Name;
+	}
+
+	public String getLocation() {
+		return mLocation;
+	}
+
+	public void setSinger(String mLocation) {
+		mLocation = Location;
+	}
+
+	public int getScore() {
+		return mScore;
+	}
+
+	public void setScore(int mScore) {
+		mScore = Score;
+	}
+  
+  //private int mScore
+  //private int mCreationDate
+}
